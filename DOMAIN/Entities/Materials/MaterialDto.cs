f301676bc6423a86c86c13a6dd5701f8fb48e387
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2024-09-23 15:01:35 +0000

    Update MaterialDto.cs

--- DOMAIN/Entities/Materials/MaterialDto.cs
+++ DOMAIN/Entities/Materials/MaterialDto.cs
@@ -4,6 +4,7 @@ namespace DOMAIN.Entities.Materials;
 
 public class MaterialDto
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public CollectionItemDto MaterialCategory { get; set; }
