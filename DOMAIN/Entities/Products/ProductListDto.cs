5c65a85179015b86a062f71d7935bf190a49cd95
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2024-10-01 10:34:49 +0000

    updates

--- DOMAIN/Entities/Products/ProductListDto.cs
+++ DOMAIN/Entities/Products/ProductListDto.cs
@@ -4,6 +4,7 @@ namespace DOMAIN.Entities.Products;
 
 public class ProductListDto
 {
+    public Guid Id { get; set; }
     public string Code { get; set; } 
     public string Name { get; set; }
     public string Description { get; set; }
