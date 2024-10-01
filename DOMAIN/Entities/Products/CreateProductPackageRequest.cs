3cfb6e7f6b34b89bc914efa79c7751d99df5d244
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2024-10-01 15:54:57 +0000

    Update CreateProductPackageRequest.cs

--- DOMAIN/Entities/Products/CreateProductPackageRequest.cs
+++ DOMAIN/Entities/Products/CreateProductPackageRequest.cs
@@ -4,7 +4,9 @@ namespace DOMAIN.Entities.Products;
 
 public class CreateProductPackageRequest
 {
+    [Required]
     public Guid MaterialId { get; set; }
+    [Required]
     public Guid PackageTypeId { get; set; }
     [StringLength(255, ErrorMessage = "Should be 255 characters or less")] public string MaterialThickness { get; set; }
     [StringLength(255,  ErrorMessage = "Should be 255 characters or less")] public string OtherStandards { get; set; }
