06eac0ee6c62a281877c07cb58d00e0439517862
lemuel.addy <lemuel.addy@hubtel.com>
2025-02-11 21:35:15 +0000

    "Added Migrations"

---
+++ INFRASTRUCTURE/Migrations/20250211211138_Addded_ArivedAt_ShipmentDocument.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INFRASTRUCTURE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addded_ArivedAt_ShipmentDocument : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArrivedAt",
+                table: "ShipmentDocuments",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArrivedAt",
+                table: "ShipmentDocuments");
+        }
+    }
+}
