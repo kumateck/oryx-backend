082b45517223a5e822393360aa5088ac485aff1b
lemuel.addy <lemuel.addy@hubtel.com>
2025-02-11 21:03:46 +0000

    "Added MarkShipmentArrived endpoint"

--- APP/Repository/ProcurementRepository.cs
+++ APP/Repository/ProcurementRepository.cs
@@ -800,5 +800,20 @@ public class ProcurementRepository(ApplicationDbContext context, IMapper mapper,
         return mapper.Map<List<MaterialDto>>(materials);
     }
 
+    public async Task<Result> MarkShipmentAsArrived(Guid shipmentDocumentId, Guid userId)
+    {
+        var shipmentDocument = await context.ShipmentDocuments.FirstOrDefaultAsync(sd => sd.Id == shipmentDocumentId);
+        if (shipmentDocument is null)
+        {
+            return Error.NotFound("ShipmentDocument.NotFound", "Shipment document not found");
+        }
 
+        shipmentDocument.ArrivedAt = DateTime.UtcNow;
+        shipmentDocument.LastUpdatedById = userId;
+
+        context.ShipmentDocuments.Update(shipmentDocument);
+        await context.SaveChangesAsync();
+
+        return Result.Success();
+    }
 }
