cbab1cd71a7ea82622e16933e2813c6ea14c7b81
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2025-02-13 08:41:43 +0000

    add is frozen

---
+++ INFRASTRUCTURE/Migrations/20250213084105_AddIsFrozenColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INFRASTRUCTURE.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsFrozenColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFrozen",
+                table: "MaterialBatches",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFrozen",
+                table: "MaterialBatches");
+        }
+    }
+}
