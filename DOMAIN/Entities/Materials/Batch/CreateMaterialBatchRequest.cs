75fb57f6c57b46a08fcd8b2b4a343f1dfef604b9
lemuel.addy <lemuel.addy@hubtel.com>
2025-02-16 14:02:42 +0000

    minor

--- DOMAIN/Entities/Materials/Batch/CreateMaterialBatchRequest.cs
+++ DOMAIN/Entities/Materials/Batch/CreateMaterialBatchRequest.cs
@@ -6,7 +6,6 @@ public class CreateMaterialBatchRequest
 {
     [StringLength(255)] public string Code { get; set; } 
     public Guid MaterialId { get; set; }         // ID of the material being added
-    public Guid ChecklistId { get; set; }        // ID of the checklist used to verify the material batch
     public decimal Quantity { get; set; }            // Quantity of the material batch
     public Guid InitialLocationId { get; set; }  // ID of the location where the batch is first stored (e.g., warehouse)
     public DateTime DateReceived { get; set; }   // Date when the material batch was received
