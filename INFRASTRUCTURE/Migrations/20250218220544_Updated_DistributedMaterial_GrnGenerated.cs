aafddbbdacc2ad17baf69b5bd875b2ca9ac1ecfe
lemuel.addy <lemuel.addy@hubtel.com>
2025-02-18 22:16:45 +0000

    "Added Migrations"

---
+++ INFRASTRUCTURE/Migrations/20250218220544_Updated_DistributedMaterial_GrnGenerated.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INFRASTRUCTURE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated_DistributedMaterial_GrnGenerated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "GrnGenerated",
+                table: "DistributedRequisitionMaterials",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GrnGenerated",
+                table: "DistributedRequisitionMaterials");
+        }
+    }
+}
