98f31afdac6e4491f44dc350df1315f3269c38bd
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2025-03-13 07:52:17 +0000

    updates

--- API/Database/Seeds/TableSeeders/OperationSeeder.cs
+++ API/Database/Seeds/TableSeeders/OperationSeeder.cs
@@ -1,7 +1,6 @@
 using APP.Utils;
 using DOMAIN.Entities.Base;
 using INFRASTRUCTURE.Context;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Database.Seeds.TableSeeders;
 
@@ -10,46 +9,17 @@ public class OperationSeeder : ISeeder
     public void Handle(IServiceScope scope)
     {
         var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+        if (dbContext.Operations.Any()) return;
+
         SeedOperations(dbContext);
     }
 
     private static void SeedOperations(ApplicationDbContext dbContext)
     {
-        var existingOperations = dbContext.Operations.ToDictionary(o => o.Name, o => o);
-        var operationList = OperationUtils.All();
+        var operations = OperationUtils.All().Select(operation => new Operation { Name = operation.Name, Description = operation.Description, Order = operation.Order}).ToList();
 
-        var operationsToUpdate = new List<Operation>();
-        var newOperations = new List<Operation>();
-
-        foreach (var operation in operationList)
-        {
-            if (existingOperations.TryGetValue(operation.Name, out var existingOperation))
-            {
-                // Update existing operation (even if order is the same)
-                existingOperation.Order = operation.Order;
-                existingOperation.Description = operation.Description;
-                operationsToUpdate.Add(existingOperation);
-            }
-            else
-            {
-                // Add new operation
-                newOperations.Add(new Operation
-                {
-                    Name = operation.Name,
-                    Description = operation.Description,
-                    Order = operation.Order
-                });
-            }
-        }
-
-        // Apply updates and insert new operations
-        if (operationsToUpdate.Count != 0)
-            dbContext.Operations.UpdateRange(operationsToUpdate);
-
-        if (newOperations.Count != 0)
-            dbContext.Operations.AddRange(newOperations);
-
-        if (operationsToUpdate.Count != 0 || newOperations.Count != 0)
-            dbContext.SaveChanges();
+        dbContext.Operations.AddRange(operations);
+        dbContext.SaveChanges();
     }
 }
