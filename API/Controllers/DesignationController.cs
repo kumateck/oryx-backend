126a20b251c3b0856557bc4d894c15aef682f939
Berndoe <88336484+Berndoe@users.noreply.github.com>
2025-04-07 18:26:16 +0000

    designation controller route naming

--- API/Controllers/DesignationController.cs
+++ API/Controllers/DesignationController.cs
@@ -1,13 +1,20 @@
 using APP.Extensions;
 using APP.IRepository;
 using DOMAIN.Entities.Designations;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+[Route("api/v{version:apiVersion}/designation")]
+[ApiController]
 public class DesignationController(IDesignationRepository repository): ControllerBase
 {
 
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Guid))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IResult> CreateDesignation([FromBody] CreateDesignationRequest request)
     {
         var userId = (string)HttpContext.Items["Sub"];
@@ -15,5 +22,48 @@ public class DesignationController(IDesignationRepository repository): Controlle
         
         var result = await repository.CreateDesignation(request, Guid.Parse(userId));
         return result.IsSuccess ? TypedResults.Ok(result.Value) : result.ToProblemDetails();
+    }
+
+    [HttpGet]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<DesignationDto>))]
+    public async Task<IResult> GetDesignations()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet("{id:Guid}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DesignationDto))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IResult> GetDesignation([FromRoute] Guid id)
+    {
+        var userId = (string)HttpContext.Items["Sub"];
+        if (userId == null) return TypedResults.Unauthorized();
+        
+        var result = await repository.GetDesignation(id);
+        return result.IsSuccess ? TypedResults.Ok(result.Value) : result.ToProblemDetails();
+    }
+
+    [HttpPut("{id:Guid}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DesignationDto))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IResult> UpdateDesignation([FromRoute] Guid id, [FromBody] CreateDesignationRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:Guid}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IResult> DeleteDesignation([FromRoute] Guid id)
+    {
+        var userId = (string)HttpContext.Items["Sub"];
+        if (userId == null) return TypedResults.Unauthorized();
+        
+        var result = await repository.DeleteDesignation(id, Guid.Parse(userId));
+        return result.IsSuccess ? TypedResults.NoContent() : result.ToProblemDetails();
     }
 }
