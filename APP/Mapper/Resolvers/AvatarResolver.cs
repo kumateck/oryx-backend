fac0edda874abe53bc8c48d4642b1073917b62a1
Berndoe <88336484+Berndoe@users.noreply.github.com>
2025-04-19 13:57:05 +0000

    adding image for employees resolver

--- APP/Mapper/Resolvers/AvatarResolver.cs
+++ APP/Mapper/Resolvers/AvatarResolver.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using DOMAIN.Entities.Employees;
 using DOMAIN.Entities.Users;
 using Microsoft.AspNetCore.Http;
 
@@ -7,6 +8,14 @@ namespace APP.Mapper.Resolvers;
 public class AvatarResolver(IHttpContextAccessor request) : IValueResolver<User, UserDto, string>
 {
     public string Resolve(User source, UserDto destination, string destMember, ResolutionContext context)
+    {
+        return string.IsNullOrEmpty(source.Avatar) ? null : $"https://{request.HttpContext?.Request.Host}/api/v1/file/avatar/{source.Avatar}";
+    }
+}
+
+public class EmployeeAvatarResolver(IHttpContextAccessor request) : IValueResolver<Employee, EmployeeDto, string>
+{
+    public string Resolve(Employee source, EmployeeDto destination, string destMember, ResolutionContext context)
     {
         return string.IsNullOrEmpty(source.Avatar) ? null : $"https://{request.HttpContext?.Request.Host}/api/v1/file/avatar/{source.Avatar}";
     }
