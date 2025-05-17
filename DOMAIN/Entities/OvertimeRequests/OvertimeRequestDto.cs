c2d998739f705c5eb238ead27c5779a418d9941c
Berndoe <88336484+Berndoe@users.noreply.github.com>
2025-05-17 01:10:36 +0000

    fix: overtime dto

--- DOMAIN/Entities/OvertimeRequests/OvertimeRequestDto.cs
+++ DOMAIN/Entities/OvertimeRequests/OvertimeRequestDto.cs
@@ -1,8 +1,10 @@
+using DOMAIN.Entities.Base;
+using DOMAIN.Entities.Departments;
 using DOMAIN.Entities.Employees;
 
 namespace DOMAIN.Entities.OvertimeRequests;
 
-public class OvertimeRequestDto
+public class OvertimeRequestDto : BaseDto
 {
     public List<EmployeeDto> Employees { get; set; }
     
@@ -10,9 +12,19 @@ public class OvertimeRequestDto
     
     public DateTime StartDate { get; set; }
     
+    public string StartTime { get; set; }
+    
     public DateTime EndDate { get; set; }
     
+    public string EndTime { get; set; }
+    
+    public OvertimeStatus Status { get; set; }
+    
     public string Justification { get; set; }
+    
+    public Guid DepartmentId { get; set; }
+    
+    public Department Department { get; set; }
     
     public int TotalHours { get; set; }
 }
