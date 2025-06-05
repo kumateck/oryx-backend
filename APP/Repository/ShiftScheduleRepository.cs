4164035dab24f44e59e066a98ec04c3dd4cac6fa
Berndoe <88336484+Berndoe@users.noreply.github.com>
2025-06-05 09:13:11 +0000

    employee id reference change

--- APP/Repository/ShiftScheduleRepository.cs
+++ APP/Repository/ShiftScheduleRepository.cs
@@ -146,7 +146,7 @@ public class ShiftScheduleRepository(ApplicationDbContext context, IMapper mappe
 
                 Employees = g.Select(s => new MinimalEmployeeInfoDto
                 {
-                    EmployeeId = s.EmployeeId,
+                    EmployeeId = s.Employee.Id,
                     FirstName = s.Employee.FirstName,
                     LastName = s.Employee.LastName,
                     StaffNumber = s.Employee.StaffNumber,
