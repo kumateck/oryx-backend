479ba8dcb1c33294e0f27b4646708bd2e77d0a88
Berndoe <88336484+Berndoe@users.noreply.github.com>
2025-06-25 17:31:50 +0000

    changing material kind type from integer

--- APP/IRepository/IMaterialStandardTestProcedureRepository.cs
+++ APP/IRepository/IMaterialStandardTestProcedureRepository.cs
@@ -1,4 +1,5 @@
 using APP.Utils;
+using DOMAIN.Entities.Materials;
 using DOMAIN.Entities.MaterialStandardTestProcedures;
 using SHARED;
 
@@ -8,7 +9,7 @@ public interface IMaterialStandardTestProcedureRepository
 {
     Task<Result<Guid>> CreateMaterialStandardTestProcedure(CreateMaterialStandardTestProcedureRequest request);
     
-    Task<Result<Paginateable<IEnumerable<MaterialStandardTestProcedureDto>>>> GetMaterialStandardTestProcedures(int page, int pageSize, string searchQuery, int materialKind);
+    Task<Result<Paginateable<IEnumerable<MaterialStandardTestProcedureDto>>>> GetMaterialStandardTestProcedures(int page, int pageSize, string searchQuery, MaterialKind materialKind);
     
     Task<Result<MaterialStandardTestProcedureDto>> GetMaterialStandardTestProcedure(Guid id);
     
