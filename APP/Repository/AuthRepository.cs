85a9ab538d9805df5abf8548414a45b322aa11ea
Tony Gyan <anthonygyan@gmail.com>
2025-07-07 16:01:09 +0000

    test email check.

--- APP/Repository/AuthRepository.cs
+++ APP/Repository/AuthRepository.cs
@@ -49,7 +49,7 @@ public class AuthRepository(IEmailService emailService,ApplicationDbContext cont
             {
                 Message = "test notification"
             });
-            //emailService.SendMail("anthonygyan@gmail.com", "Test Email", "Test Email", []);
+            emailService.SendMail("anthonygyan@gmail.com", "Test Email", "Test Email", []);
             return await jwtService.Authenticate(user, "web");
         }
         catch (Exception)
