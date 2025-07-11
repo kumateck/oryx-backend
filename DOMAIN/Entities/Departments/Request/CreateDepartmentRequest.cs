9c2426978e5b676796dfd357be3505608693a48d
Tony Gyan <anthonygyan@gmail.com>
2025-07-11 15:43:43 +0000

    add parent department id to department.

--- DOMAIN/Entities/Departments/Request/CreateDepartmentRequest.cs
+++ DOMAIN/Entities/Departments/Request/CreateDepartmentRequest.cs
@@ -9,5 +9,6 @@ public class CreateDepartmentRequest
     [StringLength(255)] public string Name { get; set; }
     [StringLength(1000)] public string Description { get; set; }
     public DepartmentType Type { get; set; }
+    public Guid? ParentDepartmentId { get; set; }
     public List<CreateWarehouseRequest> Warehouses { get; set; } = [];
 }
