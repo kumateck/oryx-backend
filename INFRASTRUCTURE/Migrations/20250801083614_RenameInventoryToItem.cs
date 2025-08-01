6f0275120ee8560b58357821a903c393b746e547
Berndoe <88336484+Berndoe@users.noreply.github.com>
2025-08-01 08:36:54 +0000

    inventory-item migration

---
+++ INFRASTRUCTURE/Migrations/20250801083614_RenameInventoryToItem.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INFRASTRUCTURE.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameInventoryToItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MaterialName = table.Column<string>(type: "text", nullable: true),
+                    Code = table.Column<string>(type: "text", nullable: true),
+                    Classification = table.Column<int>(type: "integer", nullable: false),
+                    InventoryTypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ItemTypeId = table.Column<Guid>(type: "uuid", nullable: true),
+                    UnitOfMeasureId = table.Column<Guid>(type: "uuid", nullable: false),
+                    HasBatch = table.Column<bool>(type: "boolean", nullable: false),
+                    Remarks = table.Column<string>(type: "text", nullable: true),
+                    ReorderRule = table.Column<int>(type: "integer", nullable: false),
+                    InitialStockQuantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    DepartmentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    NonProductionSupplierId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedById = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastUpdatedById = table.Column<Guid>(type: "uuid", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    LastDeletedById = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Items_InventoryTypes_ItemTypeId",
+                        column: x => x.ItemTypeId,
+                        principalTable: "InventoryTypes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Items_NonProductionSuppliers_NonProductionSupplierId",
+                        column: x => x.NonProductionSupplierId,
+                        principalTable: "NonProductionSuppliers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Items_UnitOfMeasures_UnitOfMeasureId",
+                        column: x => x.UnitOfMeasureId,
+                        principalTable: "UnitOfMeasures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Items_users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Items_users_LastDeletedById",
+                        column: x => x.LastDeletedById,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Items_users_LastUpdatedById",
+                        column: x => x.LastUpdatedById,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_CreatedById",
+                table: "Items",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_DepartmentId",
+                table: "Items",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ItemTypeId",
+                table: "Items",
+                column: "ItemTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_LastDeletedById",
+                table: "Items",
+                column: "LastDeletedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_LastUpdatedById",
+                table: "Items",
+                column: "LastUpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_NonProductionSupplierId",
+                table: "Items",
+                column: "NonProductionSupplierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_UnitOfMeasureId",
+                table: "Items",
+                column: "UnitOfMeasureId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "uuid", nullable: true),
+                    DepartmentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InventoryTypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastDeletedById = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastUpdatedById = table.Column<Guid>(type: "uuid", nullable: true),
+                    UnitOfMeasureId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Classification = table.Column<int>(type: "integer", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    HasBatch = table.Column<bool>(type: "boolean", nullable: false),
+                    InitialStockQuantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    MaterialName = table.Column<string>(type: "text", nullable: true),
+                    NonProductionSupplierId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Remarks = table.Column<string>(type: "text", nullable: true),
+                    ReorderRule = table.Column<int>(type: "integer", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Inventories_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Inventories_InventoryTypes_InventoryTypeId",
+                        column: x => x.InventoryTypeId,
+                        principalTable: "InventoryTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Inventories_NonProductionSuppliers_NonProductionSupplierId",
+                        column: x => x.NonProductionSupplierId,
+                        principalTable: "NonProductionSuppliers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Inventories_UnitOfMeasures_UnitOfMeasureId",
+                        column: x => x.UnitOfMeasureId,
+                        principalTable: "UnitOfMeasures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Inventories_users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Inventories_users_LastDeletedById",
+                        column: x => x.LastDeletedById,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Inventories_users_LastUpdatedById",
+                        column: x => x.LastUpdatedById,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_CreatedById",
+                table: "Inventories",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_DepartmentId",
+                table: "Inventories",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_InventoryTypeId",
+                table: "Inventories",
+                column: "InventoryTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_LastDeletedById",
+                table: "Inventories",
+                column: "LastDeletedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_LastUpdatedById",
+                table: "Inventories",
+                column: "LastUpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_NonProductionSupplierId",
+                table: "Inventories",
+                column: "NonProductionSupplierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_UnitOfMeasureId",
+                table: "Inventories",
+                column: "UnitOfMeasureId");
+        }
+    }
+}
