558e237f5031316410c88ac5402300d31c5ecd08
Tony Gyan <anthonygyan@gmail.com>
2025-08-06 21:53:29 +0000

    fix vendor error.

--- DOMAIN/Entities/Vendors/CreateVendorRequest.cs
+++ DOMAIN/Entities/Vendors/CreateVendorRequest.cs
@@ -11,6 +11,7 @@ public class CreateVendorRequest
     [Required, EmailAddress] public string Email { get; set; }
     [Required] public Guid CountryId { get; set; }
     [Required] public Guid CurrencyId { get; set; }
-    [Required, MinLength(1, ErrorMessage="At least one vendor item must be selected")] 
-    public List<Guid> VendorItemIds { get; set; }
+
+    [Required, MinLength(1, ErrorMessage = "At least one vendor item must be selected")]
+    public List<Guid> ItemIds { get; set; } = [];
 }
