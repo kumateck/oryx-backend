5ed8495620854678724604889af6d67446afe43a
Berndoe <88336484+Berndoe@users.noreply.github.com>
2025-08-09 17:07:34 +0000

    Update VendorController.cs

--- API/Controllers/VendorController.cs
+++ API/Controllers/VendorController.cs
@@ -2,6 +2,7 @@ using APP.Extensions;
 using APP.IRepository;
 using APP.Utils;
 using DOMAIN.Entities.Vendors;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
