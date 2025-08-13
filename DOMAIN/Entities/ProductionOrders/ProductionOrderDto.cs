5e03458d1ba0982a9dfae0cb1cc905cf0ffc3042
Tony Gyan <anthonygyan@gmail.com>
2025-08-13 19:59:54 +0000

    remove customer id.

--- DOMAIN/Entities/ProductionOrders/ProductionOrderDto.cs
+++ DOMAIN/Entities/ProductionOrders/ProductionOrderDto.cs
@@ -6,7 +6,6 @@ namespace DOMAIN.Entities.ProductionOrders;
 public class ProductionOrderDto : BaseDto
 {
     public string Code { get; set; }
-    public Guid CustomerId { get; set; }
     public CustomerDto Customer { get; set; }
     public List<ProductionOrderProductsDto> Products { get; set; } = [];
     public decimal TotalValue { get; set; }
